0d991af56d18bdd35dcbc2bd3a9cc496dff8b184
Greenox <saydulla2410@gmail.com>
2025-11-19 15:58:33 +0500

    Solve Climbing Stairs (Task 70)

--- lib/tasks/climbing_stairs.dart
+++ lib/tasks/climbing_stairs.dart
@@ -1,17 +1,7 @@
+import 'package:leetcode_solutions/utils/math_utils.dart';
+
 class ClimbingStairs {
   int climbStairs(int n) {
-    return _fibonachi(n + 1);
-  }
-
-  int _fibonachi(int n) {
-    int a = 0, b = 1;
-
-    for (int i = 2; i <= n; i++) {
-      int c = a + b;
-      a = b;
-      b = c;
-    }
-
-    return b;
+    return MathUtils.fibonachi(n + 1);
   }
 }
---
+++ lib/utils/math_utils.dart
@@ -0,0 +1,15 @@
+class MathUtils {
+  MathUtils._();
+
+  static int fibonachi(int n) {
+    int a = 0, b = 1;
+
+    for (int i = 2; i <= n; i++) {
+      int c = a + b;
+      a = b;
+      b = c;
+    }
+
+    return b;
+  }
+}
